eee1e962051c1643e34f2f273617d9aa53ef10e0
alexvitkir <alexvitkir@gmail.com>
2024-05-05 20:17:24 +0300

    вывод компонентов через итератор

--- src/components/SystemElement/MainColumn/TheIndex.vue
+++ src/components/SystemElement/MainColumn/TheIndex.vue
@@ -5,10 +5,7 @@
         <div class="row"><div class="col" @click="hide()">При клике будет исчезать колонка Роль1</div></div>
         <div class="row">
           <RoleAndContent msg="Роль 1" :class="{hidden:hidden == true}"/>
-          <RoleAndContent msg="Роль 2" />
-          <RoleAndContent msg="Роль 3" />
-          <RoleAndContent msg="Роль 4" />
-          <RoleAndContent msg="Роль 5" />
+          <RoleAndContent v-for="num in [2,3,4,5,6]" :key="num" :msg="`Роль `+num"/>
         </div>
       </div>
     </div>
