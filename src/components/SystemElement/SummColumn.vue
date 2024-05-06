d28ce506b18b701c3d6fb7a0ffc801a47397f1b9
alexvitkir <alexvitkir@gmail.com>
2024-05-06 05:33:47 +0300

    колонка "суммы" исчезает по клику на свою шапку

--- src/components/SystemElement/SummColumn.vue
+++ src/components/SystemElement/SummColumn.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="col-1 blue">
+    <div class="col-1 blue" :class="{hidden:hidden == true}">
         <div class="row">
-            <div class="col rotated">Система 1</div>
+            <div class="col rotated" @click="hide()">Система 1</div>
         </div>
         <div class="row"><div class="col cell">1</div></div>
         <div class="row"><div class="col cell">1</div></div>
@@ -13,6 +13,18 @@
     </div>
 </template>
 
+<script setup>
+    import {ref} from 'vue'
+    //import RoleAndContent from "./RoleAndContent.vue";
+
+    const hidden = ref(false);
+    
+    function hide() {
+        console.log("hide");
+        hidden.value = !hidden.value;
+    }
+</script>
+
 <style>
     .rotated {
         writing-mode: vertical-lr;
@@ -21,5 +33,9 @@
 
     .cell {
         border: solid 1px grey
+    }
+
+    .hidden{
+        display: none;
     }
 </style>
