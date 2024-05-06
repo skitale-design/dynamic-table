dc78cf9aedc7c41166c012fbf6ae9eff0f364e71
alexvitkir <alexvitkir@gmail.com>
2024-05-06 05:37:11 +0300

    вся колонка RoleAndContent скрывается по клику на шапку

--- src/components/SystemElement/MainColumn/TheIndex.vue
+++ src/components/SystemElement/MainColumn/TheIndex.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="col grey">
+  <div class="col grey" :class="{hidden:hidden == true}">
     <div class="row">
       <div class="col">
-        <div class="row"><div class="col" @click="hide()">При клике будет исчезать колонка Роль1</div></div>
+        <div class="row"><div class="col" @click="hide()">Кликни и вся колонка скроется</div></div>
         <div class="row">
-          <RoleAndContent msg="Роль 1" :class="{hidden:hidden == true}"/>
+          <RoleAndContent msg="Роль 1" />
           <RoleAndContent v-for="num in [2,3,4,5,6]" :key="num" :msg="`Роль `+num"/>
         </div>
       </div>
