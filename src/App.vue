64a950938bc6c1b7a6ad2221a69fb9e4c6140d04
alexvitkir <alexvitkir@gmail.com>
2024-05-20 18:16:23 +0300

    ДАННЫЕ СИСТЕМЫ ИЗ JSON
    Хочу больше уровней в json-файле.
    
    Добавил в data.json еще одну систему. Теперь это список систем, у каждой из которых есть id, имя и список ячеек.
    
    В основном файле пока вызывается только первая система.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-for="el in data.cells" :key="el.id">
+    <div v-for="el in data.systems[0].cells " :key="el.id">
       <OnlyDivs
         :msg=el.value
         class="cell"
@@ -30,22 +30,10 @@
   color: #2c3e50;
   margin-top: 60px;
 }
-.red {
-  background-color: rgb(255, 186, 186);
-}
-.blue {
-  background-color: rgb(184, 207, 255);
-}
-.green {
-  background-color: rgb(175, 255, 164);
-}
-.grey {
-  background-color: rgb(202, 202, 202);
-}
-.purple {
-  background-color: rgb(229, 190, 255);
-}
-.yellow {
-  background-color: rgb(223, 213, 155);
-}
+.red {background-color: rgb(255, 186, 186);}
+.blue {background-color: rgb(184, 207, 255);}
+.green {background-color: rgb(175, 255, 164);}
+.grey {background-color: rgb(202, 202, 202);}
+.purple {background-color: rgb(229, 190, 255);}
+.yellow {background-color: rgb(223, 213, 155);}
 </style>
