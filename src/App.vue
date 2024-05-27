af8802b005a8a19897d60dc7645cd5b9489457f2
Александр Ильин <alexvitkir@gmail.com>
2024-05-27 11:41:03 +0300

    GPT - попробовал генерировать код через OpenAI

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,11 @@
 <template>
   <div>
-    <div v-for="u in users.jsondata" :key="u.id">
-      <OnlyDivs :msg="u.name" :top=u.id*100 color="#B7FFE9"/>
-      <div v-for="s in u.system " :key="s.id">
-        <OnlyDivs :msg="s.name" :top="0" :left="s.id*100" color="#98cbff"/>
-        <div v-for="r in s.role " :key="r.id">
-          <OnlyDivs :msg="r.name" :top="50" :left="r.id*100" :height="50" color="yellow"  width=33 />
-          <OnlyDivs :msg="r.value" :top="u.id*100" :left="r.id*100" :height="50" color="#F3D5FF" />
-        </div>
-      </div>
-    </div>
+    <TheGpt/>
   </div>
 </template>
 
 <script setup>
-  import users from "./data/users.json";
-  import OnlyDivs from "./components/OnlyDivs/OnlyDivs.vue";
-  // function ObjectToColor(obj){
-  //     return `rgb(${obj.r},${obj.g},${obj.b})` //todo есть специальный метод для вывода в формате RGB?
-  // }
+  import TheGpt from "./components/Gpt/TheGpt.vue";
 </script>
 
 <style>
---
+++ src/components/Gpt/TheGpt.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Имя</th>
+          <th>Система</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in jsondata" :key="item.id">
+          <td>{{ item.id }}</td>
+          <td>{{ item.name }}</td>
+          <td>
+            <ul>
+              <li v-for="system in item.system" :key="system.id">
+                <p>{{ system.name }}</p>
+                <ul>
+                  <li v-for="role in system.role" :key="role.id">
+                    {{ role.name }}
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jsondata: [
+        {
+          id: 1,
+          name: "Иванов Иван Иванович",
+          system: [
+            {
+              id: 1,
+              name: "Система 1",
+              role: [
+                { id: 1, name: "Администратор", value: 1 },
+                { id: 2, name: "Пользователь", value: 0 },
+                { id: 3, name: "Гость", value: 0 },
+              ],
+            },
+            {
+              id: 2,
+              name: "Система 2",
+              role: [
+                { id: 1, name: "Бухгалтер", value: 1 },
+                { id: 2, name: "Менеджер", value: 0 },
+                { id: 3, name: "Аудитор", value: 1 },
+              ],
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: "Петров Петр Петрович",
+          system: [
+            {
+              id: 1,
+              name: "Система 1",
+              role: [
+                { id: 1, name: "Администратор", value: 1 },
+                { id: 2, name: "Пользователь", value: 0 },
+                { id: 3, name: "Гость", value: 0 },
+              ],
+            },
+            {
+              id: 2,
+              name: "Система 2",
+              role: [
+                { id: 1, name: "Бухгалтер", value: 1 },
+                { id: 2, name: "Менеджер", value: 0 },
+                { id: 3, name: "Аудитор", value: 1 },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
